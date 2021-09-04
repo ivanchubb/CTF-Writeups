eee6d4247810dd6d424760c486b9850b89c0f1e4
ivanchubb <ivanchubb@yahoo.com>
2021-09-04 19:38:51 -0400

    maths

--- CryptoHack/General/Mathematics.py
+++ CryptoHack/General/Mathematics.py
@@ -7,5 +7,39 @@ from math import gcd
 # coprime is when GCD of 2 nums is 1
 # Euclids Algorithm can calc GCD of 2 ints
 a= 66528
-b=52920
-print(gcd(a,b))
+b= 52920
+
+def mygcd(a, b):
+    # edge case
+    if a%b ==0:
+        return b
+    a1 = a
+    b1 = b
+    tmp = []
+    remainder = []
+    while True:
+        tmp.append(a1//b1)
+        remainder.append(a1%b1)
+        a1 = b1
+        b1 = remainder[-1]
+        if b1 ==0:
+            break
+    tmp.pop()
+    remainder.pop()
+    return remainder[-1]
+
+
+# Python program for the extended Euclidean algorithm
+def egcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = egcd(b % a, a)
+        return gcd, y - (b // a) * x, x
+
+
+gcd,x,y = egcd(26513,32321)
+print("The GCD is", gcd)
+print(f"x = {x}, y = {y}")
+
+print(8146798528947%17)
