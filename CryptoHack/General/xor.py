174a0024edb7c63eaaed7364b7075d753efbd138
ivanchubb <ivanchubb@yahoo.com>
2021-09-04 15:44:43 -0400

    xor

--- CryptoHack/General/xor.py
+++ CryptoHack/General/xor.py
@@ -2,6 +2,8 @@ from Crypto.Util.number import *
 from pwn import *
 import binascii
 import base64
+from PIL import Image, ImageChops
+
 word = "label"
 answer =""
 for i in word:
@@ -19,11 +21,29 @@ KEY2 ^ KEY1 = 37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e
 KEY2 ^ KEY3 = c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1
 FLAG ^ KEY1 ^ KEY3 ^ KEY2 = 04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf
 '''
-key1 = b'a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313'.hex()
-print(key1)
-key2 = xor((b'a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313'.hex()),b'37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e'.hex())
-key3 = xor(key2,b'c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1'.hex())
-flag = xor(xor(xor(b'04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf'.hex(),key3),key2),key1)
-#print(binascii.b2a_hex(flag))
-#print((bytes_to_long(flag).decode("utf-8")))
-print(long_to_bytes(flag))
+# binascii.a2b_hex will convert the byte string to bytehex so it will xor with hex vals instead of with ascii vals
+key1 = binascii.a2b_hex(b'a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313')
+#print(key1)
+key2 = xor(key1, binascii.a2b_hex(b'37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e'))
+#print(key2)
+key3 = xor(key2, binascii.a2b_hex(b'c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1'))
+#print(key3)
+flag = xor(xor(xor(binascii.a2b_hex(b'04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf'),key3),key2),key1)
+print(flag)
+
+given = b'73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d'
+givenb = binascii.a2b_hex(given)
+#print(givenb)
+key = xor('c','s')
+print(xor(givenb,key))
+
+given = b'0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104'
+givenb = binascii.a2b_hex(given)
+#got "crypto{" from the flag type, and brute forced the last char
+key = xor(givenb,b'crypto{1')[:8]
+print(xor(key,givenb))
+
+im1 = Image.open(r"CryptoHack\General\flag.png")
+im2 = Image.open(r"CryptoHack\General\lemur.png")
+im3 = ImageChops.subtract(im2,im1)
+im3.show()
